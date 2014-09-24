3d74924b85c86112a7d96bb20259cc5ffb9780ad
Ben Kirwin <ben@kirw.in>
2014-09-24 14:33:02 -0400

    Flow -> Stream

--- src/main/scala/com/monovore/coast/Graph.scala
+++ src/main/scala/com/monovore/coast/Graph.scala
@@ -1,7 +1,5 @@
 package com.monovore.coast
 
-import scala.collection.{GenTraversable, GenTraversableOnce}
-
 case class Name[A, B](name: String)
 
 /**
@@ -27,21 +25,21 @@ case class Graph[A](state: Map[String, Element[_, _]], contents: A) {
 
 sealed trait Element[A, +B]
 
-sealed trait Flow[A, +B] extends Element[A, B] {
+sealed trait Stream[A, +B] extends Element[A, B] {
 
-  def flatMap[B0](func: B => Seq[B0]): Flow[A, B0] = Transform(this, func)
+  def flatMap[B0](func: B => Seq[B0]): Stream[A, B0] = Transform(this, func)
 
-  def filter(func: B => Boolean): Flow[A, B] = flatMap { a =>
+  def filter(func: B => Boolean): Stream[A, B] = flatMap { a =>
     if (func(a)) Seq(a) else Seq.empty
   }
 
-  def map[B0](func: B => B0): Flow[A, B0] = flatMap(func andThen { b => Seq(b)})
+  def map[B0](func: B => B0): Stream[A, B0] = flatMap(func andThen { b => Seq(b)})
 
   def pool[B0](init: B0)(func: (B0, B) => B0): Pool[A, B0] = Scan(this, init, func)
 
   def latestOr[B0 >: B](init: B0): Pool[A, B0] = Scan(this, init, { (_, b: B0) => b })
 
-  def groupBy[A0](func: B => A0): Flow[A0, B] = GroupBy(this, func)
+  def groupBy[A0](func: B => A0): Stream[A0, B] = GroupBy(this, func)
 
   def groupByKey[A0, B0](implicit asPair: B <:< (A0, B0)) =
     this.groupBy { _._1 }.map { _._2 }
@@ -49,22 +47,22 @@ sealed trait Flow[A, +B] extends Element[A, B] {
   def flatten[B0](implicit func: B => Traversable[B0]) = this.flatMap(func andThen { _.toSeq })
 }
 
-case class Source[A, +B](source: String) extends Flow[A, B]
+case class Source[A, +B](source: String) extends Stream[A, B]
 
-case class Transform[A, +B, B0](upstream: Flow[A, B0], transformer: B0 => Seq[B]) extends Flow[A, B]
+case class Transform[A, +B, B0](upstream: Stream[A, B0], transformer: B0 => Seq[B]) extends Stream[A, B]
 
-case class Merge[A, +B](upstreams: Seq[Flow[A, B]]) extends Flow[A, B]
+case class Merge[A, +B](upstreams: Seq[Stream[A, B]]) extends Stream[A, B]
 
-case class GroupBy[A, B, A0](upstream: Flow[A0, B], groupBy: B => A) extends Flow[A, B]
+case class GroupBy[A, B, A0](upstream: Stream[A0, B], groupBy: B => A) extends Stream[A, B]
 
-case class PoolStream[A, B](pool: Pool[A, B]) extends Flow[A, B]
+case class PoolStream[A, B](pool: Pool[A, B]) extends Stream[A, B]
 
 
 sealed trait Pool[A, B] extends Element[A, B] {
 
   def init: B
 
-  def stream: Flow[A, B] = PoolStream(this)
+  def stream: Stream[A, B] = PoolStream(this)
 
   def map[B0](function: B => B0): Pool[A, B0] = // Mapped(this, function)
     this.stream.map(function).latestOr(function(this.init))
@@ -82,7 +80,7 @@ sealed trait Pool[A, B] extends Element[A, B] {
 
 case class Static[A, B](init: B, name: String) extends Pool[A, B]
 
-case class Scan[A, B, B0](upstream: Flow[A, B0], init: B, reducer: (B, B0) => B) extends Pool[A, B]
+case class Scan[A, B, B0](upstream: Stream[A, B0], init: B, reducer: (B, B0) => B) extends Pool[A, B]
 
 case class Mapped[A, B, B0](upstream: Pool[A, B0], mapper: B0 => B) extends Pool[A, B] {
   def init: B = mapper(upstream.init)
@@ -90,11 +88,11 @@ case class Mapped[A, B, B0](upstream: Pool[A, B0], mapper: B0 => B) extends Pool
 
 object Graph {
 
-  def merge[A, B](upstreams: Flow[A, B]*): Flow[A, B] = Merge(upstreams)
+  def merge[A, B](upstreams: Stream[A, B]*): Stream[A, B] = Merge(upstreams)
 
-  def source[A,B](name: Name[A,B]): Flow[A, B] = Source(name.name)
+  def source[A,B](name: Name[A,B]): Stream[A, B] = Source(name.name)
 
-  def label[A, B](name: String)(flow: Flow[A, B]): Graph[Flow[A, B]] = {
+  def label[A, B](name: String)(flow: Stream[A, B]): Graph[Stream[A, B]] = {
     Graph(Map(name -> flow), Source(name))
   }
 
@@ -103,7 +101,7 @@ object Graph {
     Graph(Map(name -> flow), Static(flow.init, name))
   }
 
-  def sink[A, B](name: Name[A, B])(flow: Flow[A, B]): Graph[Unit] = {
+  def sink[A, B](name: Name[A, B])(flow: Stream[A, B]): Graph[Unit] = {
     Graph(Map(name.name -> flow), ())
   }
 }
