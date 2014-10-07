1f64e2a7745b2c8f9e9182feef13a48612aedb54
Ben Kirwin <ben@kirw.in>
2014-10-06 20:56:35 -0400

    WIP

--- core/src/main/scala/com/monovore/coast/mini/actors.scala
+++ core/src/main/scala/com/monovore/coast/mini/actors.scala
@@ -1,14 +1,16 @@
 package com.monovore.coast
 package mini
 
-import akka.actor.{ActorRef, Actor}
+import java.util.concurrent.ConcurrentHashMap
+
+import akka.actor.{Actor, ActorRef}
 
 case class Checkpoint(data: Any)
 case object Readey
 case object Next
-case class NewMessage(data: Any)
+case class NewMessage(key: Any, value: Any)
 
-class SpoutActor(spout: String, log: Log[String, Any]) extends Actor { actor =>
+class SpoutActor(spout: String, log: Log[String, Any -> Any]) extends Actor { actor =>
 
   var offset: Int = 0
 
@@ -30,10 +32,81 @@ class SpoutActor(spout: String, log: Log[String, Any]) extends Actor { actor =>
 
       val next = log.fetch(spout, actor.offset)
 
-      next foreach { data =>
-        downstream ! NewMessage(data)
+      next foreach { case (k -> v) =>
+
+        downstream ! Checkpoint(actor.offset)
+        downstream ! NewMessage(k, v)
+
+        actor.offset += 1
+
         context.self ! Next
       }
+    }
+  }
+}
+
+class TransformActor[S, A, B0, B](upstream: ActorRef, init: S, transformer: A => (S, B0) => (S, Seq[B])) extends Actor { actor =>
+
+  val stateMap: ConcurrentHashMap[A, S] = new ConcurrentHashMap[A, S]()
+
+  override def receive = bootstrap
+
+  def bootstrap: Receive = {
+    case Checkpoint(offset: Int) => upstream ! Checkpoint(offset)
+    case Readey => context.become(process(context.sender()))
+  }
+
+  def process(downstream: ActorRef): Receive = {
+    case NewMessage(key: A @unchecked, value: B0 @unchecked) => {
+      val state = Option(stateMap.get(key)) getOrElse init
+      val (newState, output) = transformer(key)(state, value)
+      stateMap.put(key, newState)
+      output.foreach { newValue => downstream ! NewMessage(key, newValue) }
+    }
+  }
+}
+
+class SinkActor[A, B](
+  upstream: ActorRef,
+  checkpoints: String,
+  sink: String,
+  highWater: Int,
+  log: Log[String, Any -> Any]
+) extends Actor { actor =>
+
+  var checkpointOffset: Int = 0
+
+  var sinkOffset: Int = 0
+
+  override def receive = bootstrap
+
+  def bootstrap: Receive = {
+    case Next => {
+
+      val next = log.fetch(checkpoints, checkpointOffset)
+
+      next match {
+        case Some((key: Int) -> value) => {
+          upstream ! Checkpoint(value)
+          sinkOffset += key
+          checkpointOffset += 1
+        }
+        case None => {
+          upstream ! Readey
+          context.become(process)
+        }
+      }
+    }
+  }
+
+  def process: Receive = {
+    case NewMessage(key: A, value: B) => {
+
+      if (sinkOffset >= highWater) {
+        log.push(sink, key -> value)
+      }
+
+      sinkOffset += 1
     }
   }
 }
--- doc/mini.md
+++ doc/mini.md
@@ -3,3 +3,20 @@ coast mini
 
 `mini` is a local runner for flows that's intended to model some of the
 failure scenarios you find in a real distributed system.
+
+akka
+===
+
+The 'obvious' implementation requires a lot of message passing; I'd like to see
+if akka makes it easier to write this out.
+
+Compiling down the AST, not clear if the actors should be spinning up their own
+children or someone else should do the wiring. Points include:
+
+- Supervision: do we want to knock over the whole system when a node crashes, or
+  can we recover? Is it easier to get the right failure semantics when we hold
+  all the nodes at the top level, or just the roots?
+
+- Completion: pulling everyone to the top level makes it easier to check for
+  completion, since we already have a topological sort. Tracing it through
+  doesn't seem to be too painful, though.
