f98063ccb87cb45e3287543d10dc969a5719111b
Ben Kirwin <ben@kirw.in>
2014-11-02 17:10:42 -0500

    Remove broken example
    This job requires a cycle in the graph, which is not supported yet.

--- core/src/test/scala/com/monovore/coast/example/ExampleSpec.scala
+++ core/src/test/scala/com/monovore/coast/example/ExampleSpec.scala
@@ -4,57 +4,9 @@ package example
 import com.monovore.coast
 import org.specs2.mutable._
 
-import format.javaSerialization._
-
 class ExampleSpec extends Specification {
-
-  "a distributed entity resolution flow" should {
-
-    case class Entity(name: String, tags: Set[String], priceRange: (Double, Double))
-
-    def scope(entity: Entity): Seq[Int] = {
-      val (low, high) = entity.priceRange
-      low.toInt to high.toInt
-    }
-
-    def matches(a: Entity, b: Entity): Boolean = {
-      a == b // TODO: real matching
-    }
-
-    def merge(a: Entity, b: Entity): Entity = {
-      val (mins, maxs) = Seq(a.priceRange, b.priceRange).unzip
-      Entity(a.name, a.tags ++ b.tags, mins.min -> maxs.max)
-    }
-
-    val entities = coast.Name[String, Entity]("entities")
-
-    val merged = coast.Name[Int, Entity]("merged")
-
-//    val graph = for {
-//
-//      // Group input into the correct scopes
-//      scoped <- Flow.label("bucketed") {
-//        Flow.source(entities)
-//          .flatMap { entity => scope(entity).map { _ -> entity } }
-//          .groupByKey
-//      }
-//
-//      // Take all 'new' entities and (re)merge them
-//      // Note the circular definition here, as entities created by the merge
-//      // get piped back in.
-//      _ <- Flow.sink(merged) {
-//        Flow.merge(scoped, Flow.source(merged))
-//          .transform(Set.empty[Entity]) { (entities, nextEntity) =>
-//            // TODO: real swoosh
-//            (entities + nextEntity) -> Seq.empty
-//          }
-//          .flatMap { entity => scope(entity).map { _ -> entity } }
-//          .groupByKey
-//      }
-//    } yield ()
-
-    "do nothing" in true
-  }
+  
+  import format.javaSerialization._
 
   "a denormalized indexing implementation" should {
 
