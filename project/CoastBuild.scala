a2e026e38f7325dbf642c6776141d796754f3d52
Ben Kirwin <ben@kirw.in>
2015-05-16 23:54:44 -0400

    Bintray publishing info

--- project/CoastBuild.scala
+++ project/CoastBuild.scala
@@ -2,7 +2,9 @@ import sbt._
 import Keys._
 
 object CoastBuild extends Build {
-  
+
+  import bintray.Plugin._
+
   lazy val coast = Project(
     id = "coast",
     base = file(".")
@@ -14,7 +16,10 @@ object CoastBuild extends Build {
     scalaVersion  in ThisBuild := "2.10.4",
     scalacOptions in ThisBuild := Seq("-feature", "-language:higherKinds"),
 
-    organization in ThisBuild := "com.monovore.coast",
+    organization in ThisBuild := "com.monovore",
+    version in ThisBuild := "0.2.0-SNAPSHOT",
+
+    licenses in ThisBuild += ("Apache-2.0", url("https://www.apache.org/licenses/LICENSE-2.0.html")),
 
     // make it possible to cancel forked processes with ctrl-c
     cancelable in Global := true,
@@ -27,14 +32,14 @@ object CoastBuild extends Build {
       "org.scalacheck" %% "scalacheck" % "1.12.1" % "test"
     ),
 
-    publishTo in ThisBuild := Some(
-      Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository"))
-    )
+    publishMavenStyle := false
   )
 
   lazy val core = Project(
     id = "coast-core",
     base = file("core")
+  ) settings (
+    bintrayPublishSettings: _*
   )
 
   lazy val samza = Project(
@@ -42,5 +47,9 @@ object CoastBuild extends Build {
     base = file("samza")
   ) dependsOn (
     core
-  ) configs( IntegrationTest ) settings ( Defaults.itSettings : _*)
+  ) configs(
+    IntegrationTest
+  ) settings (
+    bintrayPublishSettings ++ Defaults.itSettings : _*
+  )
 }
