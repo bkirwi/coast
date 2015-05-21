086f5cf490d0e327d044bc2c3bbe35c027a309a7
Ben Kirwin <ben@kirw.in>
2015-05-21 00:52:21 -0400

    New connected-components implementation and test

--- core/src/main/scala/com/monovore/example/coast/ConnectedComponents.scala
+++ core/src/main/scala/com/monovore/example/coast/ConnectedComponents.scala
@@ -2,74 +2,118 @@ package com.monovore.example.coast
 
 import com.monovore.coast.core.Transformer
 import com.monovore.coast.flow.{Flow, GroupedStream}
-import com.monovore.coast.wire.{BinaryFormat, Partitioner}
+import com.monovore.coast.wire.BinaryFormat
 
 import scala.collection.immutable.SortedSet
 
 object ConnectedComponents {
 
+  import com.monovore.coast.wire.pretty._
+
+  type NodeID = Long
+
   /**
-   * Given a stream of graph edges, returns a stream of (node, component) pairs.
+   * A node is either the label for its component, or labelled with another node
+   * in the same component.
    */
-  def canonicalize[A : Ordering : BinaryFormat : Partitioner](
-    stream: GroupedStream[A, A]
-  ): Flow[GroupedStream[A, A]] = {
+  sealed trait LabelState
+  case class IsLabel(others: SortedSet[NodeID] = SortedSet.empty) extends LabelState
+  case class HasLabel(label: NodeID) extends LabelState
 
-    val ord = implicitly[Ordering[A]]
-    import ord.mkOrderingOps
+  /**
+   * We send a couple events whenever we pick a new label for a node: one grouped
+   * under the node id that contains the new label, and one grouped under the
+   * label with the new node for that component.
+   */
+  sealed trait Event
+  case class ConnectedTo(other: NodeID) extends Event
+  case class SetLabel(label: NodeID) extends Event
 
-    sealed trait LabelState
-    case class IsLabel(others: SortedSet[A] = SortedSet.empty) extends LabelState
-    case class HasLabel(label: A) extends LabelState
+  implicit val labelStateFormat = BinaryFormat.javaSerialization[LabelState]
+  implicit val eventFormat = BinaryFormat.javaSerialization[Event]
 
-    sealed trait Event
-    case class NewLabel(label: A) extends Event
-    case class ConnectedTo(other: A) extends Event
+  /**
+   * Given a stream of graph edges, returns a stream of (node, component label) pairs,
+   * where the label for a component is the smallest id of any node in that component.
+   *
+   * The edges in the input stream should be keyed by the larger of the two ids.
+   */
+  def findComponents(
+    stream: GroupedStream[NodeID, NodeID]
+  ): Flow[GroupedStream[NodeID, NodeID]] = {
 
-    // TODO: parameterize this on the format for A
-    implicit val labelStateFormat = BinaryFormat.javaSerialization[LabelState]
-    implicit val eventFormat = BinaryFormat.javaSerialization[Event]
+    /**
+     * This cycle is the engine of the algorithm, which is loosely based on the
+     * standard union-find approach. Since we need to write out a new message
+     * every time the label for some node changes, we collapse the union and
+     * find operations together.
+     *
+     * In the first phase, we send a series of ConnectedTo messages, 'walking'
+     * up the tree to find the root for each of the two nodes. If they have
+     * different roots, phase two involves setting all the nodes in one component
+     * to the newly-discovered minimum.
+     */
+    val components = Flow.cycle[NodeID, Event]("component-loop") { components =>
 
-    for {
+      Flow.merge("input" -> stream.map(ConnectedTo), "components" -> components)
+        .zipWithKey
+        .transformWith(IsLabel(): LabelState) {
 
-      components <- Flow.cycle[A, Event]("components") { components =>
+          def relabelComponent(members: SortedSet[NodeID], newLabel: NodeID) = {
 
-        Flow.merge("input" -> stream, "components" -> components.collect { case ConnectedTo(other) => other })
-          .zipWithKey
-          .transformWith(IsLabel(): LabelState) {
+            val relabeled =
+              members.toSeq.map { member => newLabel -> ConnectedTo(member) }
 
-            Transformer.onInput { case (source, target) =>
+            Transformer.setState[LabelState](HasLabel(newLabel)) andThen
+              Transformer.outputEach(relabeled)
+          }
 
+          Transformer.onInput {
+            case (source, ConnectedTo(target)) => {
               if (source == target) Transformer.skip
-              else Transformer.onState {
-                case HasLabel(repr) => {
-                  if (target == repr) Transformer.skip
-                  else if (target < repr) {
-                    Transformer.setState[LabelState](HasLabel(target)) andThen
-                      Transformer.output(repr -> ConnectedTo(target), source -> NewLabel(target))
+              else if (source > target) {
+                Transformer.onState {
+                  case HasLabel(label) => {
+                    if (target == label) Transformer.skip
+                    else if (target > label) Transformer.output(target -> ConnectedTo(label))
+                    else {
+                      Transformer.setState[LabelState](HasLabel(target)) andThen
+                        Transformer.output(label -> ConnectedTo(target))
+                    }
                   }
-                  else Transformer.output(target -> ConnectedTo(repr))
+                  case IsLabel(members) => relabelComponent(members + source, target)
                 }
+              }
+              else Transformer.onState {
+                case HasLabel(repr) => Transformer.output(repr -> ConnectedTo(target))
                 case IsLabel(members) => {
-
-                  if (target < source) {
-
-                    val relabelOthers = members.toSeq.map { _ -> ConnectedTo(target) }
-                    val relabelSelf = Seq(target -> ConnectedTo(source), source -> NewLabel(target))
-
-                    Transformer.setState[LabelState](HasLabel(target)) andThen
-                      Transformer.outputEach(relabelOthers ++ relabelSelf)
-                  }
-                  else Transformer.setState[LabelState](IsLabel(members + target))
+                  Transformer.setState[LabelState](IsLabel(members + target)) andThen
+                    Transformer.output(target -> SetLabel(source))
                 }
               }
             }
+            case (source, SetLabel(target)) => Transformer.onState {
+              case HasLabel(label) => {
+                if (label > target) Transformer.setState(HasLabel(target))
+                else Transformer.skip
+              }
+              case IsLabel(members) => {
+                sys.error(s"Tried to set label for $source to $target, but $source is already the label!")
+              }
+            }
           }
-          .groupByKey
+        }
+        .groupByKey
 
-      }
-    } yield {
-      components.collect { case NewLabel(other) => other }
+    }
+
+    components.map { stream =>
+      stream
+        .collect { case SetLabel(other) => other }
+        .transform(Long.MaxValue) { (label, update) =>
+          if (label <= update) label -> Nil
+          else update -> Seq(update)
+        }
     }
   }
 }
--- core/src/test/scala/com/monovore/example/coast/ConnectedComponentsSpec.scala
+++ core/src/test/scala/com/monovore/example/coast/ConnectedComponentsSpec.scala
@@ -2,28 +2,31 @@ package com.monovore.example.coast
 
 import com.monovore.coast.flow.{Flow, Topic}
 import com.monovore.coast.machine.{Machine, Messages, Sample}
-import org.scalacheck.Prop
+import org.scalacheck.{Gen, Prop}
 import org.specs2.ScalaCheck
+import org.specs2.matcher.Parameters
 import org.specs2.mutable._
 
 class ConnectedComponentsSpec extends Specification with ScalaCheck {
+
+  implicit val scalacheck = Parameters(maxSize = 20)
 
   "connected-components finder" should {
 
     import com.monovore.coast.wire.ugly._
 
-    val Edges = Topic[Int, Int]("edges")
+    val Edges = Topic[Long, Long]("edges")
 
-    val Components = Topic[Int, Int]("more-components")
+    val Components = Topic[Long, Long]("more-components")
 
     val graph = Flow.sink(Components) {
-      ConnectedComponents.canonicalize(Flow.source(Edges))
+      ConnectedComponents.findComponents(Flow.source(Edges))
     }
 
     "find correct label for linear graph" in {
 
       val input = Messages.from(Edges, Map(
-        1 -> Seq(0), 2 -> Seq(1), 3 -> Seq(2)
+        1L -> Seq(0L), 2L -> Seq(1L), 3L -> Seq(2L)
       ))
 
       val testCase = Machine.compile(graph).push(input)
@@ -32,6 +35,40 @@ class ConnectedComponentsSpec extends Specification with ScalaCheck {
 
         output(Components)(2).last must_== 0
         output(Components)(1).last must_== 0
+      }
+    }
+
+    "label a small random graph correctly" in {
+
+      val gen = for {
+        x <- Gen.choose(0L, 9L)
+        n <- Gen.choose(1, 3)
+        ys <- Gen.listOfN(n, Gen.choose(0L, x-1))
+      } yield (x -> ys)
+
+      Prop.forAll(Gen.mapOf(gen)) { inputs =>
+
+        val input = Messages.from(Edges, inputs)
+        val testCase = Machine.compile(graph).push(input)
+
+        Prop.forAll(Sample.complete(testCase)) { output =>
+
+          def label(id: Long) =
+            output(Components).get(id)
+              .flatMap { _.lastOption }
+              .getOrElse(id)
+
+          // if two nodes were connected, they should get the same label
+          foreach(inputs) { case (source, targets) =>
+            foreach(targets) { target =>
+              label(source) must_== label(target)
+            }
+          }
+
+          foreach(output(Components)) { case (source, labels) =>
+            labels.reverse must beSorted
+          }
+        }
       }
     }
   }
